465bd46e20b9bfbb41c0e24f23a78fbc8440367e
VishakhaSainani-Josh <vishakha.sainani@joshsoftware.com>
2025-10-29 15:25:19 +0530

    add post result api

--- api/submitResult.js
+++ api/submitResult.js
@@ -5,5 +5,5 @@ export default async function handler(req, res) {
 
   console.log("📩 Match result received:", req.body);
 
-  return res.status(200).json({ status: "ok", message: "Result received successfully" });
+  return res.status(200).json({ status: "ok", message: req.body });
 }
